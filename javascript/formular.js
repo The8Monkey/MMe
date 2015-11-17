5934e3538c8d93a7c7ac21a1ad88c7fc2a4f9bb1
Sebastian <nieling.s@gmail.com>
2015-11-17 13:00:13 +0100

    shizzle

--- javascript/formular.js
+++ javascript/formular.js
@@ -7,10 +7,35 @@ function formularChecker() {
     // If x is Not a Number or less than one or greater than 10
     if (isNaN(x) || x < 10000 || x > 15000) {
         text = "your address is not in berlin";
+        document.getElementbyId("plz").style.backgroundColor = "red";
     } else {
         text = "address OK";
     }
     console.log(text);
 
     document.getElementById("text").innerHTML = text;
+}
+//setTimeout(function(){
+//    window.location='../index.html';
+//}, 5000);
+function checkPLZ() {
+    var element = document.getElementById("plz");
+    var plz = element.value;
+    var text;
+    if (plz == "" | plz < 10000 | plz > 15000) {
+        text = "your PLZ is not in berlin";
+        element.style.backgroundColor = "#f39c12";
+    } else {
+        element.style.backgroundColor = "";
+        text = "PLZ OK";
+    }
+    console.log(text);
+
+    document.getElementById("text").innerHTML = text;
+}
+
+function checkEmail() {
+    var element = document.getElementById("email");
+    var plz = element.value;
+    var text;
 }
