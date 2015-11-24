46c3dc4178558af4d9f2e2632b0500d74cc5d378
Christoph Stumpe <ch.stumpe@web.de>
2015-11-24 13:34:07 +0100

    javascrip angefangen

--- javascript/map.js
+++ javascript/map.js
@@ -1,12 +1,21 @@
-/**
- * Created by Christoph on 17.11.2015.
- */
 
 function initialize() {
     var mapProp = {
-        center:new google.maps.LatLng(52.499268,13.4065647),
-        zoom:11,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
+        center: new google.maps.LatLng(52.499268, 13.4065647),
+        zoom: 11,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+    new google.maps.Marker({
+        map: map,
+        position: {lat: 52.5502158, lng: 13.3501616},
+        title: 'Karate Wedding'
+    });
+
+    new google.maps.Marker({
+        map: map,
+        position: {lat: 52.5167455, lng: 13.3228049},
+        title: 'Judoclub Berlin'
+    });
 }
